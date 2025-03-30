d96d47abdef2997856508ddceb274fe8a220a378
Lucian Ghinda <1407869+lucianghinda@users.noreply.github.com>
2025-03-30 07:53:22 +0300

    Initial commit

---
+++ lib/webauthor.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "webauthor/version"
+
+module Webauthor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/webauthor/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Webauthor
+  VERSION = "0.1.0"
+end
