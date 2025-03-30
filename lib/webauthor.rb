846ec4d300fa881a828429e3ebc4924a36479c36
Lucian Ghinda <1407869+lucianghinda@users.noreply.github.com>
2025-03-30 08:32:21 +0300

    Add Zeitwerk

--- lib/webauthor.rb
+++ lib/webauthor.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-require_relative 'webauthor/version'
+require 'zeitwerk'
+
+loader = Zeitwerk::Loader.for_gem
+loader.setup
 
 module Webauthor
   class Error < StandardError; end
